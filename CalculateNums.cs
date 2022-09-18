e32844e9194af853b3151ec93bba852d2c6b7013
Babek Aghamuradov <babek.aghamuradli@gmail.com>
2022-09-18 22:36:18 +0400

    Add files via upload

---
+++ CalculateNums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidLeIntFerqi
+{
+    public class CalculateNums
+    {
+        public int Count1(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public void Count2(int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VoidLeIntFerqi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateNums CalculateNums = new CalculateNums();
+
+            int result1 = CalculateNums.Count1(3, 6);//int tipinde oldugu ucun alinan neticeni "result"a beraber edib islede bilirik .
+            Console.WriteLine(result1 * 2);
+
+            //int result2= CalculateNums.Count2(3, 6); //error =>cannot implicitly convert type 'void' to 'int' //void tipinde olan metodu basqa yerde cagirib ona islem gordure bilmerik!
+            //Console.WriteLine(result2 * 2);
+            //ona sadece deyer verib netice alia bilerik :
+            CalculateNums.Count2(3, 6);
+        }
+    }
+}
